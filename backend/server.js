37ae0aed1da68db927804a9344daa4c5aa19587f
João Gabriel <joao12558joao@gmail.com>
2026-05-17 19:19:44 +0000

    conexao mysql render

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
+const db = require("./db");
+
 const app = express();
 
 app.use(cors());
@@ -12,8 +14,31 @@ app.get("/", (req, res) => {
   res.send("Backend online!");
 });
 
+app.get("/teste-banco", async (req, res) => {
+
+  try {
+
+    const [rows] = await db.query(
+      "SELECT NOW() AS data"
+    );
+
+    res.json({
+      sucesso: true,
+      banco: rows
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      sucesso: false,
+      erro: error.message
+    });
+
+  }
+
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
-});
